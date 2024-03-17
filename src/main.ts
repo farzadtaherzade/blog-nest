81456268831e167d3c4bbf3f34359c7d11b15ea1
farzad <farzadps44game@gmail.com>
2024-03-17 19:41:27 +0330

    t

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,6 @@ async function bootstrap() {
   app.use(passport.initialize());
   app.use(passport.session());
   app.use(cookieParser());
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
