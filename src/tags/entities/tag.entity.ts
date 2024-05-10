93380dd484676e9080f1fa1de1ba7993a6c83034
farzad <farzadps44game@gmail.com>
2024-05-11 03:14:58 +0330

    update tag

--- src/tags/entities/tag.entity.ts
+++ src/tags/entities/tag.entity.ts
@@ -1,21 +1,29 @@
-import { Post } from "src/posts/entities/post.entity";
-import { Column, CreateDateColumn, Entity, Index, JoinTable, ManyToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Post } from 'src/posts/entities/post.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Index,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class Tag {
-    @PrimaryGeneratedColumn()
-    id: number
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({ unique: true })
-    @Index({ fulltext: true })
-    name: string
+  @Column({ unique: true })
+  @Index({ fulltext: true })
+  name: string;
 
-    @ManyToMany(type => Post, post => post.tags)
-    posts: Post[]
+  @ManyToMany((type) => Post, (post) => post.tags)
+  posts: Post[];
 
-    @CreateDateColumn()
-    createdAt: Date
+  @CreateDateColumn()
+  createdAt: Date;
 
-    @UpdateDateColumn()
-    updatedAt: Date
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
